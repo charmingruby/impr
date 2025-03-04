b913d217c91858fc008686278aebcf38566eb186
charmingruby <mrrubycanal3105@gmail.com>
2025-03-04 17:54:41 -0300

    feat: creates polls simple migrations

--- service/poll/db/migration/000001_polls.up.sql
+++ service/poll/db/migration/000001_polls.up.sql
@@ -1,8 +1,29 @@
 CREATE TABLE IF NOT EXISTS polls
 (
-	id varchar PRIMARY KEY NOT NULL,
-	name varchar NOT NULL,
+    id varchar PRIMARY KEY NOT NULL,
+    name varchar NOT NULL,
+    description text,
+	status varchar NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp
+);
+
+CREATE TABLE IF NOT EXISTS poll_options 
+(
+    id varchar PRIMARY KEY NOT NULL,
+    content varchar NOT NULL,
+    poll_id varchar NOT NULL,
 	created_at timestamp DEFAULT now() NOT NULL,
-	updated_at timestamp,
-	deleted_at timestamp
+    
+	FOREIGN KEY (poll_id) REFERENCES polls(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS votes
+(
+    id varchar PRIMARY KEY NOT NULL,
+    poll_option_id varchar NOT NULL,
+    user_id varchar NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    
+	FOREIGN KEY (poll_option_id) REFERENCES poll_options(id) ON DELETE CASCADE
 );
